40153cc72b52e4e3406ca48ea1214d0798377c39
Martin Han <martin.m.han@gmail.com>
2020-11-25 16:47:01 -0800

    edit db scripts and rename scripts directory

--- scripts/db/upgrade.sh
+++ scripts/db/upgrade.sh
@@ -1,13 +1,11 @@
-# Use this script to run all the MongoDB scripts in the /scripts folder in order
+# Use this script to run all the MongoDB scripts in order
 # Structural changes to the database should only be done via this script
-# Scripts should be labeled and numbered in order (with the appropriate leading 0s)
+# Scripts should be labeled and numbered in order with the appropriate leading 0s
 # Scripts should be idempotent, i.e., running all of them multiple times in order should not cause any problems.
 
 set -e
 
-source ../.env;
-
-cd scripts
+source ../../.env;
 
 for FILE in `ls *.js | sort -V`; do
   mongo $FILE --eval "const dbName='"$DB_NAME"'";
@@ -15,4 +13,5 @@ for FILE in `ls *.js | sort -V`; do
 done;
 
 echo "Database updated successfully";
+
 exit 0;
